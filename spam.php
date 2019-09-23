1a1a4a4d34c082986231a27fad040402461e13e2
Muhammad Rizki <47301364+zenkriztao@users.noreply.github.com>
2019-09-23 09:34:10 +0700

    Update spam.php

--- spam.php
+++ spam.php
@@ -11,16 +11,29 @@ function send($phone){
         curl_close($ch);
                 echo $asw."\n";
 }
-echo "[----------------------------]\n\n";
-echo "Coded by Muhammad Rizki\n\n";
-echo "[----------------------------]\n\n";
-echo "COPYRIGHT SMANPOEL\n\n";
-echo "[----------------------------]\n\n";
-echo "Team Ryus4n\n\n";
-echo "[----------------------------]\n\n";
-echo "ryusan.dev@outlook.com/ t.me/cy6ny/Rizki\n\n";
-echo "[----------------------------]\n\n";
-echo "Nomor\nMasukkan Input(62xxxxxxxxx) : ";
+$red    = "\e[91m";
+echo $red."
+╔═╗┌─┐┌─┐┌┬┐  ╔═╗┌─┐┬  ┬  
+╚═╗├─┘├─┤│││  ║  ├─┤│  │  
+╚═╝┴  ┴ ┴┴ ┴  ╚═╝┴ ┴┴─┘┴─┘\n".$red.
+"Lolos ITB 2020/ Rizki";
+echo $red."
+[!] Author   : Muhammad Rizki/Ryus4n						[!]
+[!] Telegram  : t.me/cy6ny/Rizki   [!]
+[!] github   : github.com/zenkriztao       [!]\n";
+echo $red."
+======================================
+|[!]  SMANPOEL/Teknik Informatika ITB/MIT x86_x64 lu=inux inside team [!]|
+======================================\n";
+echo "\n";
+echo "\n";
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+echo "@ [!]  Contoh : 0852***** / 62852******  [!] @\n";
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+echo " \n";
+echo " \n";
+sleep ('0.4');
+echo "x86_x64 linux@ Masukan No Hp Target #-> ";
 $nomor = trim(fgets(STDIN));
 $execute = send($nomor);
 print $execute;
